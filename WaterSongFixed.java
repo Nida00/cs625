7a12033a147c96eb243d0290e639a03eaadda975
Nida00 <sparrow02k5@gmail.COM>
2019-02-27 23:00:25 +0500

    Fixed

---
+++ WaterSongFixed.java
@@ -0,0 +1,30 @@
+public class WaterSongFixed {
+public static void main(String[] args){
+ int waternum = 9;
+ String word = "bottels";
+ while(waternum > 0)
+{
+ 
+  System.out.println(waternum + " " + word + "of water on wall");
+  System.out.println(waternum + " " + word + "of water");
+  System.out.println("take one down");
+  System.out.println("pass it around");
+  waternum = waternum-1;
+    if(waternum==1)
+{ 
+  word="bottel";
+}
+  if(waternum > 0)
+{  
+   System.out.println(waternum + " " + word + "of water on wall");
+ }
+else
+{
+  System.out.println("no more water bottels");
+ }
+}
+}
+}
+ 
+ 
+ 
