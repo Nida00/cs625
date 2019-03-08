dba1817cdf82bcfb2a94d91830ca1ba0c4acde3a
Nida00 <sparrow02k5@gmail.COM>
2019-03-09 00:21:26 +0500

    week 2 assignment

---
+++ Example3.java
@@ -0,0 +1,15 @@
+class Example3 {
+public static void main (String[] args){
+float var;
+int x;
+var=10;
+x=10;
+System.out.println("original value of var: " + var);
+System.out.println("original value of x: " + x);
+System.out.println();
+var=var/4;
+x=x/4;
+System.out.println("var after division: " + var);
+System.out.println("x after division: " + x);
+}
+}
